515415ddc5d92eb789cf841cf5e7b4421372b760
Kriger09 <89879035+Kriger09@users.noreply.github.com>
2022-01-18 18:42:30 -0600

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -8,8 +8,18 @@ function clues() {
     textSize(15)
     text("R E V B A I L A", 100,50)
     fill("lightblue")
-    text("Hint: Always changing, not constant !!", 100,70)
+    text("Pista: Siempre cambiando, nunca constante !!", 100,70)
 // add code for displaying rest of the names and the hints.
     
+    fill("white")
+    textSize(15)
+    text("N U C I T O N F", 700,150)
+    fill("lightblue")
+    text("Pista: Realiza una tarea específica !!", 700,170)
 
+    fill("white")
+    textSize(15)
+    text("S T D A A B E A", 100,250)
+    fill("lightblue")
+    text("Pista: Almacena la información !!", 100,270)
 }
